68f9b3ce33a01192100c18fef236ab96ecc69d6e
avd <avd-web@outlook.com>
2024-02-24 18:50:36 +0100

    refactor(setup, OpenApiConfig): removed JPA-auditing implementation, changed OpenApiConfig data.

--- user-auth-backend/src/main/java/com/avd/security/SecurityApplication.java
+++ user-auth-backend/src/main/java/com/avd/security/SecurityApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import static com.avd.security.user.Role.ADMIN;
 import static com.avd.security.user.Role.MANAGER;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SecurityApplication {
 
 	public static void main(String[] args) {
