4f189b8a88a09ce2bbffe3a81da7a62af8399a75
avd <avd-web@outlook.com>
2024-02-24 20:04:37 +0100

    test(Setup): manually tested security setup, authentication and authorization.

--- user-auth-backend/src/main/java/com/avd/security/SecurityApplication.java
+++ user-auth-backend/src/main/java/com/avd/security/SecurityApplication.java
@@ -13,6 +13,26 @@ import static com.avd.security.user.Role.MANAGER;
 @SpringBootApplication
 public class SecurityApplication {
 
+	/*
+	* SETUP COMPLETED: Basic backend security implementation
+	* from: https://github.com/ali-bouali/spring-boot-3-jwt-security
+	*
+	* TESTED MANUALLY security setup (2024-02-24):
+	* REGISTER:	{"firstname" : "Arjan","lastname" : "van D","email" : "avd@mail.com","password" : "TEST1234","role" : "USER"}
+	* POST http://localhost:8080/api/v1/auth/authenticate
+	* GET http://localhost:8080/api/v1/demo-controller
+	* GET http://localhost:8080/api/v1/management
+	* GET http://localhost:8080/api/v1/admin
+	* POST http://localhost:8080/api/v1/auth/refresh-token
+	*
+	* NOTES / PROBLEMS:
+	* User can be registered without a role, request requires a "role" : "USER".
+	* Users can be registered double, which breaks up the authentication process.
+	* No input validations.
+	* No error handling or error messages.
+	* No tests available for testing authentication or authorization.
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
