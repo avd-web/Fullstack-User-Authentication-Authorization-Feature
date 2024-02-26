0a9501a42fa944376a222d745c3a1c3db410be9a
avd <avd-web@outlook.com>
2024-02-26 11:34:00 +0100

    feat: Added first implementation of auth tests in AuthenticationControllerTests.java - 4-4:Tests passed

--- user-auth-backend/src/test/java/com/avd/security/api/SecurityApplicationTests.java
+++ user-auth-backend/src/test/java/com/avd/security/api/SecurityApplicationTests.java
@@ -1,9 +1,9 @@
-package com.avd.security;
+package com.avd.security.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SecurityApplicationTests {
 
 	@Test
---
+++ user-auth-backend/src/test/java/com/avd/security/api/TestUtil.java
@@ -0,0 +1,36 @@
+package com.avd.security.api;
+
+import com.avd.security.user.Role;
+import com.avd.security.user.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class TestUtil {
+    public static User createMockUser1() {
+        //Arrange
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder() {
+        };
+        //Arrange testable input-values for user1
+        String TestEmail = "user1@mail.com";
+        String TestFirstname = "user1Firstname";
+        String TestLastname = "user1Lastname";
+        String TestPassword = passwordEncoder.encode("password");
+        Role TestRole = Role.USER;
+        //Return User object via builder
+        return User.builder().email(TestEmail).firstname(TestFirstname).lastname(TestLastname).password(TestPassword).role(TestRole).build();
+    }
+
+    public static User createMockAdmin1() {
+        //Arrange
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder() {
+        };
+        //Arrange testable input-values for user1
+        String TestEmail = "admin1@mail.com";
+        String TestFirstname = "admin1Firstname";
+        String TestLastname = "admin1Lastname";
+        String TestPassword = passwordEncoder.encode("password");
+        Role TestRole = Role.ADMIN;
+        //Return User object via builder
+        return User.builder().email(TestEmail).firstname(TestFirstname).lastname(TestLastname).password(passwordEncoder.encode(TestPassword)).role(TestRole).build();
+    }
+}
