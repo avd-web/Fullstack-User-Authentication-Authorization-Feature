350acf3e60b887578f2cfe0bc93e0ec8b46f0546
avd <avd-web@outlook.com>
2024-05-12 10:54:49 +0200

    feat(userPage): add .get for user info when authenticated.

--- user-auth-frontend/src/pages/UserPage.tsx
+++ user-auth-frontend/src/pages/UserPage.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from "react";
+import Login from "../components/general/Login";
+import axios, { AxiosError } from "axios";
+
 export default function UserPage() {
-  return (
-    <>
-      <p>UserPage</p>
-    </>
+  const [authenticatedUser, setAuthenticatedUser] = useState<string | null>(
+    null
   );
+
+  useEffect(() => {
+    const token =
+      sessionStorage.getItem("access_token") ||
+      localStorage.getItem("access_token");
+    setAuthenticatedUser(token);
+  }, []);
+
+  const getUser = async () => {
+    console.log(authenticatedUser);
+    try {
+      if (!authenticatedUser) {
+        throw new Error("No authenticated user found");
+      }
+
+      const response = await axios.get(
+        "http://localhost:8080/api/v1/demo-user/user",
+        {
+          headers: {
+            Authorization: `Bearer ${authenticatedUser}`,
+          },
+        }
+      );
+      console.log("User data:", response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        if (axiosError.response && axiosError.response.status === 401) {
+          console.error("Unauthorized: Please login again.");
+          // Handle unauthorized access, maybe redirect to login page
+          return;
+        }
+      }
+      console.error("getUserError:", error);
+    }
+  };
+
+  if (!authenticatedUser) {
+    return (
+      <>
+        <Login />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <button onClick={getUser}>Get User Data</button>
+      </>
+    );
+  }
 }
